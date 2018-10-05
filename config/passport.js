124b64d251e2511f044fcf2e41c91beb18e05690
mashiro11 <thiagoaas1990@gmail.com>
2018-10-05 10:19:08 -0300

    Notebook diffs

--- config/passport.js
+++ config/passport.js
@@ -2,14 +2,18 @@ const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const mongoose = require('mongoose')
 
+// Get db user table
 const User = mongoose.model('users')
 const keys = require('./keys')
 
+// Creating options for JwtStrategy
 const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 opts.secretOrKey = keys.secretOrKey
 
 module.exports = passport => {
+  // The JWT authentication strategy is constructed as follows:
+  // new JwtStrategy(options, verify)
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     User.findById(jwt_payload.id)
         .then(user => {
@@ -22,3 +26,6 @@ module.exports = passport => {
     //id: user.id, name: user.name, avatar: user.avatar
   }))
 }
+
+//reference
+// https://www.npmjs.com/package/passport-jwt
